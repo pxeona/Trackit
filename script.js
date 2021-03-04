84a311107b925384db46754efea5f868094c2c57
Pravin Xeona <pravin.xeona21@gmail.com>
2021-03-04 11:25:11 +1100

    Set the map to load the current location upon opening the page

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-var map = L.map("map").setView([51.505, -0.09], 13);
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      const coords = [latitude, longitude];
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+      const map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+    },
+    function () {
+      alert("Could not retrieve the position");
+    }
+  );
+}
